b9bb46c8e211d15629762fb143f7f5959d263838
Basile BRUNET <bbrunet@e1r4p22.42.fr>
2020-09-29 15:36:30 +0200

    modified

--- module00/ex02/acc.cpp
+++ module00/ex02/acc.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 #include <ctime>
 
+Account::Account( int initial_deposit ) : _amount(initial_deposit) , _nbDeposits(0) , _nbWithdrawals(0), _accountIndex(Account::_nbAccounts)
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created"
+    << std::endl;
+    Account::_nbAccounts++;
+    Account::_totalAmount += initial_deposit;
+}
+
+Account::~Account( void )
+{
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed"  
+    << std::endl;
+}
+
 int	Account::getNbAccounts( void )
 {
     return Account::_nbAccounts;
@@ -29,19 +45,6 @@ void	Account::displayAccountsInfos( void )
     << std::endl;
 }
 
-Account::Account( int initial_deposit ) : _amount(initial_deposit) , _nbDeposits(0) , _nbWithdrawals(0), _accountIndex(Account::_nbAccounts)
-{
-    Account::_displayTimestamp();
-    std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created"
-    << std::endl;
-    Account::_nbAccounts++;
-}
-
-Account::~Account( void ){
-    Account::_displayTimestamp();
-    std::cout << "index:" << this->_accountIndex << ";amount:" << _amount << ";closed"  
-    << std::endl;
-}
 
 void	Account::makeDeposit( int deposit )
 {
@@ -61,7 +64,7 @@ bool	Account::makeWithdrawal( int withdrawal )
     std::cout << "index:" << this->_accountIndex << ";pamount:";
     if (withdrawal > this->_amount)
     {
-        std::cout << ";withdrawal:refused";
+        std::cout << ";withdrawal:refused" << std::endl;
         return false;
     }
     this->_amount -= withdrawal;
