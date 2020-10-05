b2591eb3f0e0bd4a470f36086c8178ff31cb1743
Basile BRUNET <bbrunet@e1r7p8.42.fr>
2020-10-05 15:41:20 +0200

    deals with /n or not at the end of the file

--- module01/ex07/main.cpp
+++ module01/ex07/main.cpp
@@ -1,36 +1,48 @@
 #include "fstream"
 #include "sstream"
 #include "iostream"
+#include "string"
+#include "locale"
+
+
+std::string to_upper(char *str)
+{
+    int i(0);
+    while (str[i])
+    {
+        str[i] = std::toupper(str[i]);
+        i++;
+    }
+    std::string str_upper(str);
+    return(str_upper);
+}
 
 int main(int ac, char **av)
 {
+    std::string line;
+    std::string find_str;
+    std::string replace_str;
+    size_t pos;
+    std::ifstream file;
+    std::ofstream file_replace;
+    int i(0);
+
     if (ac != 4)
     {
         std::cout << "Wrong number of arguments" << std::endl;
         return 1;
     }
-    std::ifstream file(av[1]);
-    std::ofstream file_replace("replace");
-    std::string str;
-
-    while (getline(file, str))
+    find_str = av[2];
+    replace_str = av[3];
+    file.open(av[1]);
+    file_replace.open(to_upper(av[1]) + ".replace");
+    while (getline(file, line))
     {
-        file_replace << str << std::endl;
+        while ((pos = line.find(find_str)) != std::string::npos)
+            line.replace(pos, find_str.length(), replace_str);
+        file_replace << line;
+        if (file.eof() == false)
+            file_replace << std::endl;
     }
-    
-
-    // std::getline(file, str1);
-    // file >> str;
-    // std::cout << str << std::endl;
-    // std::cout << file.eof() << std::endl;
-    // file >> str;
-    // std::cout << str << std::endl;
-    // std::cout << file.eof() << std::endl;
-    // file >> str;
-    // std::cout << str << std::endl;
-    // std::cout << file.eof() << std::endl;
-    // file >> str;
-    // std::cout << str << std::endl;
-    // std::cout << file.eof() << std::endl;
-
+    file.close();
 }
