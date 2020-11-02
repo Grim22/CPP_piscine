1d57c1485ab8913363b27bc65016a2d56ad43ac1
Basile <basile.brunet@gmail.com>
2020-11-02 17:51:19 +0100

    ex00 m03

---
+++ module03/ex00/FragTrap.hpp
@@ -0,0 +1,30 @@
+#ifndef FRAGTRAP_H
+# define FRAGTRAP_H
+
+#include <string>
+
+class FragTrap{
+    private:
+    unsigned int hit_points;
+    unsigned int max_hit_points;
+    unsigned int energy_points;
+    unsigned int max_energy_points;
+    unsigned int level;
+    std::string name;
+    unsigned int melee_attack_damage;
+    unsigned int ranged_attack_damage;
+    unsigned int armor_damage_reduction;
+
+    public:
+    FragTrap(const std::string &name);
+    FragTrap(const FragTrap &copy);
+    ~FragTrap(void);
+    FragTrap&  operator=(const FragTrap &copy);
+    void rangedAttack(std::string const & target);
+    void meleeAttack(std::string const & target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    void vaulthunter_dot_exe(std::string const & target);
+};
+
+#endif
