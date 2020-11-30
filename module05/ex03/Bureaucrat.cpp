f211d08f9497d80b9a841b720a1b14b904c9b23e
Basile <basile.brunet@gmail.com>
2020-11-30 10:53:43 +0100

    copied from ex02 - missing added

---
+++ module05/ex03/Bureaucrat.cpp
@@ -0,0 +1,94 @@
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+#include "Form.hpp"
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) throw(GradeTooHighException, GradeTooLowException):
+name(name), grade(grade)
+{
+    if (grade > 150)
+        throw GradeTooLowException();
+    else if (grade < 1)
+        throw GradeTooHighException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy):
+name(copy.name), grade(copy.grade)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+    //std::cout << "Destructor called" << std::endl;
+}
+
+Bureaucrat&   Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+    this->grade = rhs.grade;
+    return(*this);
+}
+
+std::ostream & operator<<(std::ostream &o, const Bureaucrat &rhs)
+{
+    o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << std::endl;
+    return o;
+}
+        
+const std::string &Bureaucrat::getName() const
+{
+    return this->name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return this->grade;
+}
+
+void Bureaucrat::incGrade() throw(GradeTooHighException)
+{
+    if (this->grade == 1)
+        throw Bureaucrat::GradeTooHighException();
+    else
+        this->grade--;
+}
+
+void Bureaucrat::decGrade() throw(GradeTooLowException)
+{
+    if (this->grade == 150)
+        throw Bureaucrat::GradeTooLowException();
+    else
+        this->grade++;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade Too High Exception caught";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade Too Low Exception caught";
+}
+
+void Bureaucrat::signForm(Form& form) const
+{
+    if (form.getIsSigned() == true)
+        std::cout << this->getName() << " cannot sign " << form.getName() << " because form is already signed" << std::endl;
+    else if (this->grade > form.getGradeSign())
+        std::cout << this->getName() << " cannot sign " << form.getName() << " because his grade is too low" << std::endl;
+    else
+        std::cout << this->getName() << " signs " << form.getName() << std::endl;
+    form.beSigned(*this);
+}
+
+void Bureaucrat::executeForm(Form const &form) const
+{
+    if (form.getIsSigned() == false)
+        std::cout << this->getName() << " cannot execute " << form.getName() << " because it is not signed" << std::endl;
+    else if (this->grade > form.getGradeSign())
+        std::cout << this->getName() << " cannot execute " << form.getName() << " because his grade is too low" << std::endl;
+    else
+        std::cout << this->getName() << " executes " << form.getName() << std::endl;
+    form.execute(*this); // call to derived-class::execute (polymorphism also works with references !)
+}
