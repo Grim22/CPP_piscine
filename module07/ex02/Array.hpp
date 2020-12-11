6267e3620d6b10e50b6501d6d300ee88512ae093
Basile <basile.brunet@gmail.com>
2020-12-11 10:01:59 +0100

    ex02 - some improvments

--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -11,21 +11,27 @@ class Array{
 	public:
 		Array(): array(new T[0]), a_size(0) // allocates an empty array (non zero amount of space ! will be deleted with [])
 		{};
-		Array(unsigned int n): array(new T[n]()), a_size(n)
+
+		Array(unsigned int n): array(new T[n]()), a_size(n) // new T[n]() : synthax for default initialization: every element in the array will be "default initialized": for classes, means that default constructor will be called; for non classes (int, floats...) zero initialization is done
 		{};
+		
 		Array(const Array &copy): a_size(copy.a_size)
 		{
 			this->array = new T[copy.a_size];
 			for (unsigned int i = 0; i < copy.a_size; i++)
 				this->array[i] = copy.array[i];
-		};
+		}
+
 		Array &operator=(const Array &rhs)
 		{
 			delete [] this->array;
+			this->array = new T[rhs.a_size];
 			for (unsigned int i = 0; i < rhs.a_size; i++)
 				this->array[i] = rhs.array[i];
-			this->a_size = rhs.a_size;	
+			this->a_size = rhs.a_size;
+			return *this;
 		}
+
 		~Array()
 		{
 			delete [] this->array;
@@ -37,6 +43,7 @@ class Array{
 				throw std::out_of_range("out of range exception thrown");
 			return this->array[n];
 		}
+
 		unsigned int size() const
 		{
 			return this->a_size;
--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -1,8 +1,11 @@
 #include "Array.hpp"
+#include "Fixed.hpp"
 #include <iostream>
 
 int main()
 {
+    // Array<int>
+    Array<int>e; // empty array
     Array<int> a(5);
     std::cout << a.size() << std::endl;
     a[0] = 10;
@@ -16,5 +19,40 @@ int main()
     {
         std::cerr << e.what() << '\n';
     }
+    // allocation
+    Array<int>b(6);
+    b[3] = 42;
+    a = b;
+    std::cout << a[3] << std::endl;
+    // copy construction
+    Array<int>c(b);
+    std::cout << c[3] << std::endl;
+    
+    std::cout << "---" << std::endl;
+
+    // Array<Fixed>
+    Array<Fixed> E;
+    Array<Fixed> A(5);
+    std::cout << A.size() << std::endl;
+    A[0] = 10;
+    std::cout << A[0] << std::endl;
+    std::cout << A[1] << std::endl;
+    try
+    {
+        A[6] = 1;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    // allocation
+    Array<Fixed>B(6);
+    B[3] = 42.42f;
+    A = B;
+    std::cout << A[3] << std::endl;
+    // copy construction
+    Array<Fixed>C(B);
+    std::cout << C[3] << std::endl;
+
     
 }
