6e88307863c2b124341ac009ab69084b6dad42a0
Basile <basile.brunet@gmail.com>
2020-12-17 15:47:46 +0100

    added main given in subject and empty .cpp

--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -55,6 +55,7 @@ int main()
 
 }
 
+// main given in subject
 // int main()
 // {
 // MutantStack<int> mstack;
@@ -66,7 +67,6 @@ int main()
 // mstack.push(3);
 // mstack.push(5);
 // mstack.push(737);
-// //[...]
 // mstack.push(0);
 // MutantStack<int>::iterator it = mstack.begin();
 // MutantStack<int>::iterator ite = mstack.end();
@@ -78,5 +78,29 @@ int main()
 // ++it;
 // }
 // std::stack<int> s(mstack);
+
+// std::cout << "---" << std::endl;
+
+// //with std::list
+// std::list<int> mstack_l;
+// mstack_l.push_back(5);
+// mstack_l.push_back(17);
+// std::cout << mstack_l.back() << std::endl;
+// mstack_l.pop_back();
+// std::cout << mstack_l.size() << std::endl;
+// mstack_l.push_back(3);
+// mstack_l.push_back(5);
+// mstack_l.push_back(737);
+// mstack_l.push_back(0);
+// std::list<int>::iterator it_l = mstack_l.begin();
+// std::list<int>::iterator ite_l = mstack_l.end();
+// ++it_l;
+// --it_l;
+// while (it_l != ite_l)
+// {
+// std::cout << *it_l << std::endl;
+// ++it_l;
+// }
+
 // return 0;
 // }
